5e91c1503c32abf6ffabeeecde60ed475515517a
Foxy Mike <michaelpatterson@ymail.com>
2022-03-16 15:29:39 -0500

    feat: adding a test comment

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+#leaving a test comment! 
+"""
+now using multi-line comment: https://www.w3schools.com/python/gloss_python_multi_line_comments.asp
+When committing for best practice follow a structure like the following:
+
+feat: <very brief summary> Comment Testing
+
+> <verb> <subject>
+> added a comment to test out source control!
+
+
+
+feat = feature
+fix = bug fix
+
+other examples of best practices:
+https://gist.github.com/robertpainsi/b632364184e70900af4ab688decf6f53
+"""
+
 print("hello world")
